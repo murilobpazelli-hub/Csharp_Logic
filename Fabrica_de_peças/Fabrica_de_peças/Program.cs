1ff2ec1c6eb39ce6605a8f145c125d9d9134b724
Murilo <murilo.b.pazelli@gmail.com>
2026-09-17 10:51:36 -0300

    More Activities

---
+++ Fabrica_de_peças/Fabrica_de_peças/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrica_de_peças
+{
+    internal class Program
+    {
+        /* Uma fábrica  tem uma linha de produção capaz de produzir 400 peças por dia.
+         * Um funcionário controla a qualidade, cadastrando o número da peça e o seu estado (aprovado ou reprovado). 
+         * Crie um programa para cadastrar o controle de qualidade e imprimir o total de peças aprovadas e reprovadas no final do dia.
+         */
+        static void Main(string[] args)
+        {
+            int dado, contador = 0, aprovado_total = 0, reprovado_total = 0;
+            string estado;
+            
+         while (contador != 40)
+            {
+                Console.WriteLine("Olá! Por favor digite o número de sua peça abaixo: \n (Caso queira finalizar o programa, aperte 0)");
+                dado = int.Parse(Console.ReadLine());
+                if (dado == 0)
+                {
+                    Console.WriteLine("Programa finalizado!\n Peças aprovadas: " + aprovado_total + "\n Peças reprovadas: " + reprovado_total);
+                    break;
+                }
+                Console.WriteLine("Certo, por favor declare o estado da peça:\n(a = aprovado/)\n(r = reprovado) ");
+                estado = Console.ReadLine();
+                if (estado == "a")
+                {
+                    aprovado_total++;
+                    contador++;
+                    Console.Clear();
+                }
+                if (estado == "r")
+                {
+                    reprovado_total++;
+                    contador++;
+                    Console.Clear();
+                }
+
+                
+                if (contador == 40)
+                {
+                    Console.WriteLine("Programa finalizado!\n Peças aprovadas: " + aprovado_total + "\n Peças reprovadas: " + reprovado_total);
+                    break;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Guessing_game/Guessing_game/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guessing_game
+{
+    internal class Program
+    {
+        /* Simular o jogo de adivinhação: o jogador 1 escolhe um número entre 1 e 10;
+         * o jogador 2 insere números na tentativa de acertar o número escolhido pelo jogador 1.
+         * Quando ele acertar, o algorítmo deve informar que ele acertou o número x (escolhido pelo jogador 1)
+         * em x tentativas (quantidade de tentativas do jogador 2).
+         */
+        static void Main(string[] args)
+        {
+            int numero1, numero2, tentativas = 1;
+            // Boolean == Lógico
+
+            Console.WriteLine("Olá! Jogador 1, por favor digite um número entre 1 e 10 para iniciar o jogo: ");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Certo. Jogador 2, por favor digite um número entre 1 e 10 para advinhar o número do Jogador 1: ");
+            numero2 = int.Parse(Console.ReadLine());
+            Console.Clear();
+            while (numero2 != numero1) {
+                Console.WriteLine("Número errado, por favor advinhe outro número: ");
+                numero2 = int.Parse(Console.ReadLine()) ;
+                tentativas++;
+                Console.Clear();
+                if (numero2 == numero1)
+                {
+                    Console.WriteLine("Você advinhou o número do Jogador 1, que é " + numero1 + "!\nNúmero de tentativas do Jogador 2: " + tentativas);
+                }
+                
+            }
+            
+        }
+        }
+    }
+
